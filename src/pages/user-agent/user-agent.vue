d1320aeb58e990faee55a417ebc8f4d08b9fd190
ZJ <zj781119282@163.com>
2018-06-11 10:52:29 +0800

    add ua

---
+++ src/pages/user-agent/user-agent.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="getUserAgent" style="font-size: 32px;padding: 20px;">点我</button>
+</template>
+
+<script>
+  export default {
+    name: 'user-agent',
+    methods: {
+      getUserAgent() {
+        alert(navigator.userAgent)
+      },
+    },
+  }
+</script>
